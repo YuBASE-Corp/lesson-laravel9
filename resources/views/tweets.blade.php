6698ed1802010ba6181f834e646d0e98774aba77
cha1ra <cha1ra.garnet@gmail.com>
2022-07-27 13:53:33 +0900

    フォームにタグ投稿機能を追加

--- resources/views/tweets.blade.php
+++ resources/views/tweets.blade.php
@@ -2,7 +2,7 @@
     <div class="container mt-4">
         <div class="row justify-content-center">
             <div class="col-12 col-sm-12 col-md-10 col-lg-8">
-                    <x-tweet-form />
+                    <x-tweet-form :tags="$tags" />
                     @foreach($tweets as $tweet)
                         <x-tweet-card :tweet="$tweet" />
                     @endforeach
